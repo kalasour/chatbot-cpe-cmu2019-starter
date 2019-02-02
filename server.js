6b6e9d37a202a6831dc21bb9a6eb20e3b6fa04ed
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-02-02 10:59:10 +0700

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express')
-
+const middleware = require('@line/bot-sdk').middleware
 const app = express()
+
+const config = {
+  channelAccessToken: 'y+ES/H/rQ7qnsxXtZuqRKvxvUfjjhmv2LDLAh6g3vSE9ZY7ZN2TnD5jQmie5lDlhA65deMnUhsjELyeirHrZ/dUbw22DQby7nH+4U6PnkJjvNvyBmOHZNTyuk6J/D87MxTWRbUekHPjhqPtIbtLHcAdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '625996762def6ffdb810d6ef869bd023'
+}
+
+app.post('/webhook', middleware(config), (req, res) => {
+  // req.body.events // webhook event objects
+  // req.body.destination // user ID of the bot (optional)
+  console.log('Webhook success!')
+})
+
+
+
+
+
 
 app.get('/', function (req, res) {
     res.send('Hello World!!')
