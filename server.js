79a229b76d965569aa9ee661392f2b9664e84a0e
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-02-02 12:01:56 +0700

    reply

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
 const express = require('express')
 const middleware = require('@line/bot-sdk').middleware
 const app = express()
+const Client = require('@line/bot-sdk').Client;
+
+
 
 const config = {
   channelAccessToken: 'y+ES/H/rQ7qnsxXtZuqRKvxvUfjjhmv2LDLAh6g3vSE9ZY7ZN2TnD5jQmie5lDlhA65deMnUhsjELyeirHrZ/dUbw22DQby7nH+4U6PnkJjvNvyBmOHZNTyuk6J/D87MxTWRbUekHPjhqPtIbtLHcAdB04t89/1O/w1cDnyilFU=',
   channelSecret: '625996762def6ffdb810d6ef869bd023'
 }
 
+
+const client = new Client(config);
+
+
 app.post('/webhook', middleware(config), (req, res) => {
   // req.body.events // webhook event objects
   // req.body.destination // user ID of the bot (optional)
   res.send('Webhook success!')
+  const event = req.body.events[0];
+  if (event.type === 'message') {
+    const message = event.message;
+    client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: message,
+    });
+  }
 })
 
-
-
-
-
-
 app.get('/', function (req, res) {
-    res.send('Hello World!!')
+  res.send('Hello World!!')
 })
 
 app.set('port', (process.env.PORT || 4000))
